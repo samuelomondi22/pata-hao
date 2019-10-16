d50a98798683178aeb5aec2387b95a8ade8c876d
samuelomondi22 <omondisam696@gmail.com>
2019-10-16 10:13:30 +0300

    correction

--- src/context.js
+++ src/context.js
@@ -33,7 +33,7 @@ class RoomProvider extends Component {
         order: 'sys.createdAt'
       });
       let rooms = this.formatData(response.items);
-      console.log(rooms);
+      // console.log(rooms);
       let featuredRooms = rooms.filter(room => room.featured === true);
       let maxPrice = Math.max(...rooms.map(item => item.price));
       let maxSize = Math.max(...rooms.map(item => item.size));
